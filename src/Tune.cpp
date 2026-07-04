58174da7d88223076ede269a7576bdaada6340cf
Greg-R <greg.electricity@gmail.com>
2026-07-04 07:24:20 -0400

    Removed last of the #define PLLMODULE code.

--- src/Tune.cpp
+++ src/Tune.cpp
@@ -219,11 +219,7 @@ void ResetFlipFlops() {
   // Toggle GPO0 low momentarily to reset the divide-by-2 flop-flops.
   // GPO0 is held high during normal operation.
   si5351.output_enable(SI5351_CLK2, 0);
-#ifdef PLLMODULE
   si5351.output_enable(SI5351_CLK0, 0);
-#else
-  si5351.output_enable(SI5351_CLK1, 0);
-#endif
   digitalWrite(0, LOW);  // Reset low.
   delay(500);
   digitalWrite(0, HIGH);  // Normal operation high.
